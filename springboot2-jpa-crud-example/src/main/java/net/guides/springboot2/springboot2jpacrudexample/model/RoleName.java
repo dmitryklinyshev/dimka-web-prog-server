04b9d1245163b39d719ac24f0e54da931ad9014f
Dmitry <klinyshev.dmitry@mail.ru>
2019-11-08 10:41:44 +0400

    initial commit server

---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/RoleName.java
@@ -0,0 +1,7 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package net.guides.springboot2.springboot2jpacrudexample.repository;
+
+
+import net.guides.springboot2.springboot2jpacrudexample.model.Role;
+import net.guides.springboot2.springboot2jpacrudexample.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
